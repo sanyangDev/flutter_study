8709efc6d98cb5cf7fd47ab0fafc2703388737f0
sanyangDev <songhyeonseo@gmail.com>
2023-07-03 00:06:37 +0900

    studying dart and flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,29 @@ import 'package:flutter/material.dart';
 typedef ListOfInt = List<int>;
 
 class Player {
-  final String name = 'seo_nue';
-  int xp = 1500;
+  final String name;
+  int xp;
+  int age;
+  int heigth;
+
+  Player({
+    required this.name,
+    required this.xp,
+    required this.age,
+    required this.heigth,
+});
 
   void sayHello(){
-    print("song love $name");
+    print("이름 : $name, 경험치 : $xp, 나이 : $age, 키 : $heigth");
   }
 }
 
 void main() {
-  var player = Player();
+  var player = Player(
+    name : "seo_nue",
+    xp : 2000,
+    age : 22,
+    heigth: 156,
+  );
   player.sayHello();
 }
