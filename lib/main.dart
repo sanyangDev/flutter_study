6bf299dcd766f151e1770ba627af3c06ff90c2b0
sanyangDev <songhyeonseo@gmail.com>
2023-07-05 21:14:36 +0900

    studying dart and flutter

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,33 @@ typedef ListOfInt = List<int>;
 
 class Player {
   final String name;
-  int xp;
-  int age;
-  int heigth;
+  int xp, age;
+  String team;
 
   Player({
     required this.name,
     required this.xp,
     required this.age,
-    required this.heigth,
+    required this.team,
 });
 
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+}) : this.age = age,
+  this.name = name,
+  this.team = 'blue',
+  this.xp = 0;
+
   void sayHello(){
-    print("이름 : $name, 경험치 : $xp, 나이 : $age, 키 : $heigth");
+    print("이름 : $name, 경험치 : $xp, 나이 : $age, 키 : $team");
   }
 }
 
 void main() {
-  var player = Player(
+  var player = Player.createBluePlayer(
     name : "seo_nue",
-    xp : 2000,
     age : 22,
-    heigth: 156,
   );
-  player.sayHello();
+  print(player);
 }
