bc83b3745ee445412e5ebf18bf544b15b1bae7de
sanyangDev <songhyeonseo@gmail.com>
2023-07-10 20:50:03 +0900

    studying dart and flutter

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,18 @@ void main() {
     '블랙핑크',
     ['지수', '로제', '제니', '리사'],
   );
-  print(blackPink.name);
-  print(blackPink.members);
-  blackPink.sayHello();
-  blackPink.introduce();
+
+  Idol bts = Idol(
+    'BTS',
+    ['RM', '뷔', '지민', '정국', '제이홉'],
+  );
 }
 
 class Idol {
-  String name;
-  List<String> members;
+  final String name;
+  final List<String> members;
 
-  Idol(String name, List<String> members)
-      : this.name = name,
-        this.members = members;
+  Idol(this.name, this.members);
 
   void sayHello(){
     print('안녕 나는 ${this.name}이야.');
@@ -25,5 +24,15 @@ class Idol {
 
   void introduce(){
     print('우리 팀은 ${this.members}로 구성되어 있어.');
+  }
+
+  //getter
+  String get firstMember{
+    return this.members[0];
+  }
+
+  //setter
+  set firstMembers(String name){
+    this.members[0] = name;
   }
 }
