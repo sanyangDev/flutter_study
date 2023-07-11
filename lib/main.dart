a7d3f934b4877045a29d430c9df15230381a37f0
sanyangDev <songhyeonseo@gmail.com>
2023-07-11 16:23:32 +0900

    studying dart and flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,21 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
-  Idol blackPink = Idol(
-    '블랙핑크',
-    ['지수', '로제', '제니', '리사'],
-  );
+  final controller = StreamController();
+  final stream = controller.stream.asBroadcastStream();
 
-  Idol bts = Idol(
-    'BTS',
-    ['RM', '뷔', '지민', '정국', '제이홉'],
-  );
-}
+  final streamListener1 = stream.where((val) => val % 2 == 0).listen((val) {
+    print('Listener1 : $val');
+  });
 
-class Idol {
-  final String name;
-  final List<String> members;
+  final streamListener2 = stream.where((val) => val % 2 != 0).listen((val){
+    print('Listener2 : $val');
+  });
 
-  Idol(this.name, this.members);
-
-  void sayHello(){
-    print('안녕 나는 ${this.name}이야.');
-  }
-
-  void introduce(){
-    print('우리 팀은 ${this.members}로 구성되어 있어.');
-  }
-
-  //getter
-  String get firstMember{
-    return this.members[0];
-  }
-
-  //setter
-  set firstMembers(String name){
-    this.members[0] = name;
-  }
+  controller.sink.add(1);
+  controller.sink.add(2);
+  controller.sink.add(3);
+  controller.sink.add(4);
+  controller.sink.add(5);
 }
