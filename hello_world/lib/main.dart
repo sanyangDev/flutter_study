7c9f9fa92c25bf0d19ef9a43d71a8ca70d628755
sanyangDev <songhyeonseo@gmail.com>
2023-07-20 23:30:44 +0900

    start

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,27 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/screen/home_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(home: HomeScreen()),
+    MaterialApp(
+      home: HomeScreen(),
+    ),
   );
 }
 
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.amberAccent,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'asset/img/rock.png',
-          ),
-          CircularProgressIndicator(
-            color: Colors.white,
-          ),
-        ],
-      ),
-    );
-  }
-}
+
