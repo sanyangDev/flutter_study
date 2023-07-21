cf54207ecff2a7806c810877e91187d895fb9bdb
sanyangDev <songhyeonseo@gmail.com>
2023-07-21 16:33:10 +0900

    start

--- hello_world/lib/screen/home_screen.dart
+++ hello_world/lib/screen/home_screen.dart
@@ -7,15 +7,38 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          bottom: false,
-          child: Container(
-      color: Colors.black,
-      child: Column(
+      bottom: false,
+      child: Container(
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              color: Colors.red,
-              width: 50.0,
-              height: 50.0,
+            // Expanded, Flexible
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+                Container(
+                  color: Colors.orange,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+                Container(
+                  color: Colors.yellow,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+                Container(
+                  color: Colors.green,
+                  width: 50.0,
+                  height: 50.0,
+                ),
+              ],
             ),
             Container(
               color: Colors.orange,
@@ -23,9 +46,31 @@ class HomeScreen extends StatelessWidget {
               height: 50.0,
             ),
             Container(
-              color: Colors.yellow,
-              width: 50.0,
-              height: 50.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                  Container(
+                    color: Colors.orange,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    width: 50.0,
+                    height: 50.0,
+                  ),
+                ],
+              ),
             ),
             Container(
               color: Colors.green,
@@ -33,8 +78,8 @@ class HomeScreen extends StatelessWidget {
               height: 50.0,
             ),
           ],
+        ),
       ),
-    ),
-        ));
+    ));
   }
 }
