f42034b4a4cbb60e35d4a8440e6b79f8553b2792
sanyangDev <songhyeonseo@gmail.com>
2023-07-23 20:18:38 +0900

    start

--- hello_world/lib/screen/home_screen.dart
+++ hello_world/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,80 +7,10 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      bottom: false,
-      child: Container(
-        color: Colors.black,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Expanded, Flexible
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 50.0,
-                  height: 50.0,
-                ),
-                Container(
-                  color: Colors.orange,
-                  width: 50.0,
-                  height: 50.0,
-                ),
-                Container(
-                  color: Colors.yellow,
-                  width: 50.0,
-                  height: 50.0,
-                ),
-                Container(
-                  color: Colors.green,
-                  width: 50.0,
-                  height: 50.0,
-                ),
-              ],
-            ),
-            Container(
-              color: Colors.orange,
-              width: 50.0,
-              height: 50.0,
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                  Container(
-                    color: Colors.orange,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                  Container(
-                    color: Colors.yellow,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                  Container(
-                    color: Colors.green,
-                    width: 50.0,
-                    height: 50.0,
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              color: Colors.green,
-              width: 50.0,
-              height: 50.0,
-            ),
-          ],
-        ),
+      body: WebView(
+        initialUrl: 'https//blog.codefactory.ai',
+        javascriptMode: JavascriptMode.unrestricted,
       ),
-    ));
+    );
   }
 }
