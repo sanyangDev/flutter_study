c9dc6bc0cfeb1d6d30b5ba0ee2872c32752143ca
sanyangDev <songhyeonseo@gmail.com>
2023-07-27 22:27:59 +0900

    start

--- hello_world/lib/screen/home_screen.dart
+++ hello_world/lib/screen/home_screen.dart
@@ -2,13 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  WebViewController? controller;
+  final homeUrl = 'https://blog.codefactory.ai/';
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Ormm'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                if(controller == null){
+                  return;
+                }
+                controller!.loadUrl(homeUrl);
+              },
+              icon: Icon(
+                Icons.home,
+              ))
+        ],
+      ),
       body: WebView(
-        initialUrl: "https://blog.codefactory.ai/",
+        onWebViewCreated: (WebViewController controller) {
+          this.controller = controller;
+        },
+        initialUrl: homeUrl,
+        javascriptMode: JavascriptMode.unrestricted,
       ),
     );
   }
