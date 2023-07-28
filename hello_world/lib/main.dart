15021f05eb53ff68fbd7b2dbf6e727fdb8802ff3
sanyangDev <songhyeonseo@gmail.com>
2023-07-28 21:57:05 +0900

    start

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:hello_world/screen/home_screen.dart';
 
 void main() {
+  // Flutter 프레임워크가 app을 실행할 준비가 될 때까지 기다린다.
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     MaterialApp(
       home: HomeScreen(),
--- hello_world/lib/screen/home_screen.dart
+++ hello_world/lib/screen/home_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai/');
+
 class HomeScreen extends StatelessWidget {
-  WebViewController? controller;
-  final homeUrl = 'https://blog.codefactory.ai/';
+  WebViewController controller = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..loadRequest(homeUrl);
 
   HomeScreen({super.key});
 
@@ -17,23 +20,23 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
               onPressed: () {
-                if(controller == null){
-                  return;
-                }
-                controller!.loadUrl(homeUrl);
+                controller.loadRequest(homeUrl);
               },
               icon: Icon(
                 Icons.home,
               ))
         ],
       ),
-      body: WebView(
-        onWebViewCreated: (WebViewController controller) {
-          this.controller = controller;
-        },
-        initialUrl: homeUrl,
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
+      body: WebViewWidget(
+        controller: controller,
+      )
+      // WebView(
+      //   onWebViewCreated: (WebViewController controller) {
+      //     this.controller = controller;
+      //   },
+      //   initialUrl: homeUrl,
+      //   javascriptMode: JavascriptMode.unrestricted,
+      // ),
     );
   }
 }
