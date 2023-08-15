5fa85e31ad93e2f1917f69c64bb530d77f4be8f7
sanyangDev <songhyeonseo@gmail.com>
2023-08-15 20:08:31 +0900

    anything

--- couple_date/lib/screen/home_screen.dart
+++ couple_date/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,60 +9,145 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.pink[100], // 100 ~ 900까지로 색상 톤 정함.
-        body: SafeArea(
+      backgroundColor: Colors.pink[100], // 100 ~ 900까지로 색상 톤 정함.
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              _TopPart(
+                selectedDate: selectedDate,
+                onPressed: onHeartPressed,
+              ),
+              _BottomPart(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  onHeartPressed () {
+    final DateTime now = DateTime.now();
+    //dialog
+    showCupertinoDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.bottomCenter,
           child: Container(
-            width: MediaQuery
-                .of(context)
-                .size
-                .width,
-            child: Column(
-              children: [
-                Text(
-                  'U&I',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'parisienne',
-                    fontSize: 80.0,
-                  ),
-                ),
-                Text(
-                  '우리 처음 만난 날',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'sunflower',
-                    fontSize: 30.0,
-                  ),
-                ),
-                Text(
-                  '2023.08.12',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'sunflower',
-                    fontSize: 20.0,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.favorite,
-                    color: Colors.white,
-                    size: 40.0,
-                  ),
-                ),
-                Text('D+1',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'sunflower',
-                    fontSize: 50.0,
-                    fontWeight: FontWeight.w700,
-                  ),),
-              ],
+            color: Colors.white,
+            height: 300.0,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: selectedDate,
+              maximumDate: DateTime(
+                now.year,
+                now.month,
+                now.day,
+              ),
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  selectedDate = date;
+                });
+              },
             ),
           ),
-        ));
+        );
+      },
+    );
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  final DateTime selectedDate;
+  final VoidCallback onPressed;
+
+  _TopPart({
+    required this.selectedDate,
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'U&I',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'parisienne',
+              fontSize: 80.0,
+            ),
+          ),
+          Column(
+            children: [
+              Text(
+                '우리 처음 만난 날',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'sunflower',
+                  fontSize: 30.0,
+                ),
+              ),
+              Text(
+                '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'sunflower',
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+          IconButton(
+            iconSize: 60.0,
+            onPressed: onPressed,
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.pink,
+            ),
+          ),
+          Text(
+            'D+${DateTime(
+                  now.year,
+                  now.month,
+                  now.day,
+                ).difference(selectedDate).inDays + 1}',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'sunflower',
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(child: Image.asset('asset/img/middle_image.png'));
   }
 }
